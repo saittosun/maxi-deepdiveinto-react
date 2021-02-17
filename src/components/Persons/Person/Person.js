af5c2d727ca723b6c39ee239765e04094fc9d43c
tostos <saittosun2@gmail.com>
2021-02-17 13:03:49 +0100

    auxiliary yerine <> </> bunu kullanabilirsin

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary';
 
 import classes from './Person.module.css';
 
 class Person extends Component {
-	render() {
-		console.log('[Person.js] is rendering...');
-		return (
-			<div className={classes.Person}>
-				<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-				<p>{this.props.children}</p>
-				<input type="text" onChange={this.props.changed} value={this.props.name} />
-			</div>
-		)
-	}
+  render() {
+    console.log('[Person.js] is rendering...');
+    return (
+      <>
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </>
+
+    )
+  }
 };
 
 export default Person;
